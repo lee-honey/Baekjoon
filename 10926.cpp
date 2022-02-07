e200b67958b59227f6f74cf4af880ef99b20c9eb
HyunHee_LEE <57757760+dimplehh@users.noreply.github.com>
2022-02-07 19:53:12 +0900

    Create 10926.cpp

---
+++ 10926.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    string id;
+    cin >> id;
+
+    cout << id;
+    cout << "??!" << '\n';
+}
