8f4f20a794dfffc523ff78618a74cbd4607a37b5
dimplehh <dimplehh@naver.com>
2022-08-17 12:25:15 +0900

    0817

---
+++ 정렬/11650_xysort.py
@@ -0,0 +1,10 @@
+import sys
+
+N = int(input())
+lst = []
+for i in range(N):
+    a,b = map(int,input().split())
+    lst.append([a,b])
+lst.sort()
+for i in range(N):
+    print(lst[i][0],lst[i][1])
