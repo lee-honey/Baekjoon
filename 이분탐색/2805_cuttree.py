cc451bba96ceecd4ec5db90fd7336350a02cdb4f
dimplehh <dimplehh@naver.com>
2022-09-06 10:55:33 +0900

    0906

---
+++ 이분탐색/2805_cuttree.py
@@ -0,0 +1,15 @@
+import sys
+N, M = map(int, input().split())
+lst = list(map(int, sys.stdin.readline().split()))
+start, end = 1, max(lst)
+while start <= end:
+    mid = (start + end) // 2
+    cnt = 0
+    for i in lst:
+        if i > mid:
+            cnt += i - mid
+    if cnt >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
