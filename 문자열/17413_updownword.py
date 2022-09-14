a3c6b85c82ecb330482178e7110622b0cad591b5
dimplehh <dimplehh@naver.com>
2022-09-14 13:46:13 +0900

    0914

---
+++ 문자열/17413_updownword.py
@@ -0,0 +1,20 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+S = input()
+lst = S.split('>')
+
+def splitHamsu(s):
+    S = s.split()
+    for i in range(len(S)):
+        for j in range(len(S[i])):
+            print(S[i][len(S[i]) - j - 1] , end = '')
+        if i != len(S) - 1:
+            print(end = ' ')
+
+for i in range(len(lst)):
+    tmp = lst[i].split('<')
+    if (len(tmp[0]) != 0):
+        splitHamsu(tmp[0])
+    if (len(tmp) == 2):
+        print('<'+tmp[1]+'>', end = '')
