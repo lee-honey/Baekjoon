775d3b5fe5ca4366c0ecf807caed5e356eea2878
dimplehh <dimplehh@naver.com>
2022-10-06 18:52:45 +0900

    1006

--- 재귀/10994_star19.py
+++ 재귀/10994_star19.py
@@ -1,7 +1,11 @@
-N = int(input())
-# def star(depth):
-#     if depth == N:
-#         return
-print('*' * 5) 
-
-#star(1)
+def add_star(ptn):
+    return "* " + ptn + " *"
+def star(n):
+    if n == 1:
+        return ["*"]
+    else:
+        return ['*' * (1 + 4 * (n - 1)), '*'+' ' * (4 * (n - 1) - 1)+'*']\
+        +list(map(add_star, star(n - 1)))\
+        +['*'+' ' * (4 * (n - 1) - 1)+'*', '*' * (1 + 4 * (n - 1))]
+print(star(int(input())))
+print("\n".join(star(int(input()))))
